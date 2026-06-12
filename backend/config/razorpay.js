315bd5b3e048f3aa61925c44e517c0ce63345a1e
sakshamjain6620 <sakshamj232@gmail.com>
2026-06-12 11:03:48 +0530

    Delete backend directory

--- backend/config/razorpay.js
+++
@@ -1,9 +0,0 @@
-const Razorpay = require('razorpay');
-require('dotenv').config();
-
-const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID || 'rzp_test_SxyTETXvDZEo7l',
-    key_secret: process.env.RAZORPAY_KEY_SECRET || 'Vx2CQK8NSd7k8wXF7NxrZMlf'
-});
-
-module.exports = razorpay;
--- backend/routes/ai.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { processAIChat, getPatientSymptomSummary } = require('../controllers/ai.controller');
-
-router.post('/chat', processAIChat);
-router.post('/summary', getPatientSymptomSummary);
-
-module.exports = router;
